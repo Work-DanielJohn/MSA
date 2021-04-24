16ee6131858c434c385edd3f4fb58d6f866a947a
work.danieljohn <work.danieljohn@gmail.com>
2021-04-24 14:14:17 +1200

    Button alternate color between red and blue

--- index.js
+++ index.js
@@ -2,4 +2,16 @@ window.onload = () => {
     btn = document.getElementById("btn");
     console.log(btn);
     btn.innerHTML = "worlddddd";
+    let btnColor = "blue";
+
+    btn.onclick = () => {
+        if (btnColor == "blue") {
+            btnColor = "red";
+            btn.style = "color : red";
+        }
+        else {
+            btnColor = "blue";
+            btn.style = "color : blue";
+        }
+    }
 }
